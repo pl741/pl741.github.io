cd60b0bdbfc6ce4b4fd06c7d4397ff1e6f5b37b5
Pan Lin <panl@bupt.edu.cn>
2020-05-01 11:11:38 +0800

    Site updated: 2020-05-01 11:11:37

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -32,6 +32,12 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
+  $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+  //默认情况下折叠
+  $("div.fold").css("display","none");
   var html = $('html');
   var TAB_ANIMATE_DURATION = 200;
   var hasVelocity = $.isFunction(html.velocity);
